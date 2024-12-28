2a609f30187abab10e0757f34cd8f257aaba8ec8
elux-io <133917886+elux-io@users.noreply.github.com>
2024-12-28 16:15:19 +0100

    c++ version macro

--- src/Lux/Core/PlatformMacros.hpp
+++ src/Lux/Core/PlatformMacros.hpp
@@ -11,7 +11,7 @@
 	#define LUX_PRIVATE_COMPILER_CLANG() 0
 #endif
 
-#if defined(__GNUC__) && !defined(__clang__)
+#if defined(__GNUC__) and not defined(__clang__)
 	#define LUX_PRIVATE_COMPILER_GCC() 1
 #else
 	#define LUX_PRIVATE_COMPILER_GCC() 0
@@ -23,9 +23,9 @@
 	#define LUX_PRIVATE_COMPILER_MSVC() 0
 #endif
 
-#if !LUX_COMPILER(CLANG) && \
-    !LUX_COMPILER(GCC) && \
-    !LUX_COMPILER(MSVC)
+#if not LUX_COMPILER(CLANG) and \
+    not LUX_COMPILER(GCC) and \
+    not LUX_COMPILER(MSVC)
 #pragma message("unknown compiler")
 #endif
 
@@ -46,14 +46,31 @@
 	#define LUX_PRIVATE_OS_LINUX() 0
 #endif
 
-#if defined(__APPLE__) && defined(__MACH__)
+#if defined(__APPLE__) and defined(__MACH__)
 	#define LUX_PRIVATE_OS_MAC() 1
 #else
 	#define LUX_PRIVATE_OS_MAC() 0
 #endif
 
-#if !LUX_OS(WINDOWS) && \
-    !LUX_OS(LINUX) && \
-    !LUX_OS(MAC)
+#if not LUX_OS(WINDOWS) and \
+    not LUX_OS(LINUX) and \
+    not LUX_OS(MAC)
 #pragma message("unknown OS")
+#endif
+
+/**
+ * C++ version detection
+ */
+#define LUX_CPP(version) LUX_PRIVATE_CPP_##version()
+
+#if __cplusplus >= 202002L
+	#define LUX_PRIVATE_CPP_20() 1
+#else
+	#define LUX_PRIVATE_CPP_20() 0
+#endif
+
+#if __cplusplus >= 202302L
+	#define LUX_PRIVATE_CPP_23() 1
+#else
+	#define LUX_PRIVATE_CPP_23() 0
 #endif
