f62c939c3ef3f2c581568a5b2d952b20e98b7db1
elux-io <133917886+elux-io@users.noreply.github.com>
2025-01-08 18:33:32 +0100

    assume

--- src/Lux/Core/Debug.hpp
+++ src/Lux/Core/Debug.hpp
@@ -5,17 +5,31 @@
 #include <Lux/Log/Log.hpp>
 
 #if LUX_COMPILER(MSVC)
-	#define LUX_DEBUG_BREAK() (__debugbreak())
+	#define LUX_DEBUG_BREAK() __debugbreak()
 #elif LUX_COMPILER(CLANG) or LUX_COMPILER(GCC)
-	#define LUX_DEBUG_BREAK() (__builtin_trap())
+	#define LUX_DEBUG_BREAK() __builtin_trap()
 #else
 	#define LUX_DEBUG_BREAK() ((void)0)
+#endif
+
+#if LUX_CPP(23)
+	#define LUX_ASSUME(expr) [[assume(expr)]]
+#else
+	#if LUX_COMPILER(MSVC)
+		#define LUX_ASSUME(expr) __assume(expr)
+	#elif LUX_COMPILER(CLANG)
+		#define LUX_ASSUME(expr) __builtin_assume(expr)
+	#elif LUX_COMPILER(GCC)
+		#define LUX_ASSUME(expr) __attribute__((assume(expr)))
+	#else
+		#define LUX_ASSUME(...) ((void)0)
+	#endif
 #endif
 
 #if not defined(NDEBUG)
 	#define LUX_ASSERT(expr, ...) ((expr) or (::lux::detail::DO_NOT_PAY_ATTENTION_TO_WHATEVER_THE_HELL_THE_COMPILER_IS_SAYING__THIS_ERROR_IS_CAUSED_BY_A_FAILED_ASSERT(), LUX_LOG_ERROR("assertion `" #expr "` failed" __VA_OPT__(": ") __VA_ARGS__), LUX_DEBUG_BREAK(), false))
 #else
-	#define LUX_ASSERT(...) ((void)0)
+	#define LUX_ASSERT(expr, ...) LUX_ASSUME(expr)
 #endif
 
 namespace lux
